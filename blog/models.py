abeeed7978fdcf98c8030f533eb12c35dc14116e
Morozyuk Daniil <morozdan2003@gmail.com>
2022-07-12 14:34:34 +0300

    Create model for post objects

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Post object model
+class Post(models.Model):
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(verbose_name="Post title" ,max_length=100)
+    contents = models.TextField(verbose_name="Post contents")
+    posted_at = models.DateTimeField(verbose_name="Posted at")
