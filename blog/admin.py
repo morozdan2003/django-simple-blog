dc43595f883f298af03203ad819873d4583ee5ee
Morozyuk Daniil <morozdan2003@gmail.com>
2022-07-12 14:35:52 +0300

    Setup the admin panel to display posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# settings for displaying the model
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'posted_at')
+
+# register post objects in admin site
+admin.site.register(Post, PostAdmin)
